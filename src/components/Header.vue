90a80ce2295bb033dfa31a1b83cd00a3461fdaab
Shellharbour-Digital <hello@shellharbour.digital>
2021-07-14 23:08:32 +1000

    implemented name and headline state in store

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,30 +2,45 @@
     <section id="header">
         <header>
             <span class="image avatar"><img src="images/avatar.jpg" alt="" /></span>
-            <h1 id="logo"><a href="#">Stefan Markovic</a></h1>
-            <p>Non col pellentesque ut cep augue at<br />
-            cep ata sed commodo quam</p>
+            <h1 id="logo"><a href="#">{{ name }}</a></h1>
+            <p><span v-for="headline in headlines" :key="headline.text">
+                {{ headline.text }}<br />
+            </span></p>
         </header>
         <nav id="nav">
             <ul>
                 <li><a href="#one" class="active">About Me</a></li>
-                <li><a href="#two">Things I Can Do</a></li>
+                <li><a href="#two" class=''>Things I Can Do</a></li>
                 <li><a href="#three">A Few Accomplishments</a></li>
                 <li><a href="#four">Contact</a></li>
             </ul>
         </nav>
         <footer>
             <ul class="icons">
-                <li><a href="#" class="icon brands fa-linkedin">
-                    <span class="label">Instagram</span>
+                <li><a target="_blank" href="https://www.linkedin.com/in/stefan-markovic-801772212/" class="icon brands fa-linkedin">
+                    <span class="label">LinkedIn</span>
                 </a></li>
-                <li><a href="#" class="icon brands fa-github">
+                <li><a target="_blank" href="https://github.com/Shellharbour-Digital" class="icon brands fa-github">
                     <span class="label">Github</span>
                 </a></li>
-                <li><a href="#" class="icon solid fa-envelope">
+                <li><a href="mailto:hello@stefanmarkovic.com" class="icon solid fa-envelope">
                     <span class="label">Email</span>
                 </a></li>
             </ul>
         </footer>
     </section>
 </template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Header',
+  computed: {
+    ...mapState({
+      name: (state) => state.name,
+      headlines: (state) => state.headline,
+    }),
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,11 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    name: 'Stefan Markovic',
+    headline: [
+      { text: 'Web Developer' },
+      { text: 'DevOps Engineer' },
+    ],
   },
   mutations: {
   },
